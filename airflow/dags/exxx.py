ddeb3e921f55fed10db984d28c212e870fa08277
adithya <sunkariadithya46@gmail.com>
2024-04-22 18:35:36 +0530

    hagfs

--- airflow/dags/exxx.py
+++ airflow/dags/exxx.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import LabelEncoder
 from io import BytesIO
 from minio import Minio
 import boto3
+import csv
+import io
 
 # Define the MongoDB connection parameters
 mongo_uri = "mongodb://root:root@mongodb:27017/"
@@ -41,8 +43,15 @@ def extract_data_from_mongodb(**kwargs):
     # Close the MongoDB connection
     client.close()
 
-    # Pass extracted data to the next task without printing it
-    kwargs['ti'].xcom_push(key='extracted_data', value=extracted_data)
+    # Convert extracted data to CSV format
+    csv_data = io.StringIO()
+    writer = csv.DictWriter(csv_data, fieldnames=extracted_data[0].keys())
+    writer.writeheader()
+    writer.writerows(extracted_data)
+
+    # Return CSV data as a string
+    return csv_data.getvalue()
+
 
 # Transformation functions
 def find_categorical_numerical_columns(df):
@@ -56,7 +65,6 @@ def fill_missing_values(df):
     df['Item_Weight'].fillna(median_weight, inplace=True)
     df['Outlet_Size'] = np.where(df['Outlet_Size'].isna(), df['Outlet_Size'].mode()[0], df['Outlet_Size']) 
     return df
-
 
 def clean_item_fat_content(df):
     df['Item_Fat_Content'] = df['Item_Fat_Content'].replace('low fat', 'Low Fat')
@@ -83,13 +91,10 @@ def drop_column(df, column_name):
     return df
 
 # Define the function to perform data transformations
-def transform_data(**kwargs):
-    # Retrieve extracted data from XCom
-    extracted_data = kwargs['ti'].xcom_pull(task_ids='extract_data_task', key='extracted_data')
-    
-    # Convert extracted data to DataFrame
-    df = pd.DataFrame(extracted_data)
-    
+def transform_data(csv_data, **kwargs):
+    # Convert CSV data to DataFrame
+    df = pd.read_csv(pd.compat.StringIO(csv_data))
+
     # Perform data processing
     categorical_columns, _ = find_categorical_numerical_columns(df)
     df = fill_missing_values(df)
@@ -98,22 +103,18 @@ def transform_data(**kwargs):
     df = one_hot_encode(df, columns=['Item_Type', 'Item_Identifier_Categories', 'Outlet_Identifier'])
     df = drop_column(df, 'Item_Identifier')
     df = drop_column(df, '_id')
-    
-     # Serialize DataFrame to JSON
-    serialized_df = df.to_json(orient='records')
-    # Pass the serialized DataFrame to the next task using XCom
-    kwargs['ti'].xcom_push(key='transformed_data', value=serialized_df)
+
+    # Save transformed data back to CSV
+    transformed_csv_data = df.to_csv(index=False)
+
+    # Pass the transformed CSV data to the next task using XCom
+    kwargs['ti'].xcom_push(key='transformed_csv_data', value=transformed_csv_data)
 
 
-# Define saving function
-def save_to_minio(transformed_data, minio_endpoint, minio_access_key, minio_secret_key, minio_bucket_name, minio_object_key):
+
+# Define a function to save transformed data to Minio
+def save_to_minio(transformed_csv_data, minio_endpoint, minio_access_key, minio_secret_key, minio_bucket_name, minio_object_key, **kwargs):
     try:
-        # Deserialize JSON string to DataFrame
-        df = pd.read_json(transformed_data, orient='records')
-
-        if not isinstance(df, pd.DataFrame):
-            raise TypeError("Argument 'df' is not a DataFrame")
-
         s3 = boto3.client(
             's3',
             endpoint_url=minio_endpoint,
@@ -123,18 +124,14 @@ def save_to_minio(transformed_data, minio_endpoint, minio_access_key, minio_secr
             config=boto3.session.Config(signature_version='s3v4')
         )
 
-        # Convert DataFrame to bytes
-        csv_buffer = BytesIO()
-        df.to_csv(csv_buffer, index=False)
-        csv_buffer.seek(0)
+        # Upload CSV data as an object to Minio bucket
+        s3.put_object(Bucket=minio_bucket_name, Key=minio_object_key, Body=transformed_csv_data.encode('utf-8'))
 
-        # Upload DataFrame as CSV file to MinIO
-        s3.put_object(Bucket=minio_bucket_name, Key=minio_object_key, Body=csv_buffer.getvalue())
-        print("Processed data uploaded to MinIO successfully.")
+        print("Transformed data uploaded to Minio successfully.")
 
     except Exception as e:
         # Log the error and mark the task as failed
-        print(f"Error occurred: {str(e)}")
+        print(f"Error occurred while uploading data to Minio: {str(e)}")
         raise
 
 
@@ -162,26 +159,30 @@ extract_data_task = PythonOperator(
 transform_data_task = PythonOperator(
     task_id='transform_data_task',
     python_callable=transform_data,
+    op_args=[
+        "{{ ti.xcom_pull(task_ids='extract_data_task') }}",  # Pass the CSV data fetched from the previous task
+    ],
     provide_context=True,
     dag=dag
 )
 
-# Define the task to save transformed data to MinIO
+# Define the task to save transformed data to Minio
 save_to_minio_task = PythonOperator(
     task_id='save_to_minio_task',
     python_callable=save_to_minio,
     op_args=[
-        '{{ task_instance.xcom_pull(task_ids="transform_data_task") }}', 
+        '{{ task_instance.xcom_pull(task_ids="transform_data_task", key="transformed_csv_data") }}', 
         minio_endpoint, 
         minio_access_key, 
         minio_secret_key, 
         minio_bucket_name, 
         minio_object_key
     ],
-    provide_context=True,  
+    provide_context=True,
     dag=dag
 )
 
+
 # Task dependencies
 extract_data_task >> transform_data_task >> save_to_minio_task
- #hjcsz
+
